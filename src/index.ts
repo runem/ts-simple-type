201924867f8ba6203a87bb0edc8a269b844f20a2
runem <runemehlsen@gmail.com>
2019-02-15 12:12:14 +0100

    feat: Add function that can return the string representation of either a native typescript type or a simple type

--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,7 @@ export * from "./is-assignable-to-primitive-type";
 export * from "./is-assignable-to-type";
 export * from "./is-assignable-to-value";
 export * from "./to-simple-type";
+export * from "./to-type-string";
 export * from "./is-assignable-to-simple-type-kind";
 export * from "./simple-type-to-string";
 export * from "./simple-type";
---
+++ src/to-type-string.ts
@@ -0,0 +1,18 @@
+import { Type, TypeChecker } from "typescript";
+import { isSimpleType, SimpleType } from "./simple-type";
+import { simpleTypeToString } from "./simple-type-to-string";
+
+/**
+ * Returns a string representation of a given type.
+ * @param simpleType
+ */
+export function toTypeString(simpleType: SimpleType): string;
+export function toTypeString(type: Type, checker: TypeChecker): string;
+export function toTypeString(type: SimpleType | Type, checker?: TypeChecker): string {
+	if (isSimpleType(type)) {
+		return simpleTypeToString(type);
+	}
+
+	// Use the typescript checker to return a string for a type
+	return checker!.typeToString(type);
+}
